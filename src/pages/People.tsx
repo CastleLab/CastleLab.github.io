71b3ab78dc1cf1bb3e7ba6746eba402ed99eb66d
zongyiLyu <lvzongyi2002@gmail.com>
2024-09-02 15:26:13 +0800

    Update People.tsx

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -333,6 +333,22 @@ export function People(): ReactElement {
             <p>PhD Student at HKUST</p>
           </div>
 
+          <div className="banner-bottom-left">
+            <a href="https://github.com/dlibk">
+              <div className="portrait">
+                <img
+                  alt=""
+                  src="images/zongyiLyu.jpg"
+                  className="portrait-round"
+                />
+              </div>
+            </a>
+            <h3>
+              <a href="https://zongyiLyu.github.io/">Zongyi Lyu</a>
+            </h3>
+            <p>PhD Student at HKUST</p>
+          </div>
+
           {/* Former Members */}
           <div className="clearfix"> </div>
           <h2>Former Members</h2>
